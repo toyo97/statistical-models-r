5a71146e5e26453862f7e79d1964e900b289a516
Vittorio Zampinetti <zampinetti@gmail.com>
2022-10-11 17:31:14 +0200

    fixed dependencies installation script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-rg -o "library\((\w+)\)" -r '$1' *.Rmd | # find all 'library' statements and get package
+rmd_lessons=./lessons
+rmd_labs=./labs
+rmd_std=.
+
+rmdFiles=()
+for p in ${!rmd_@}; do
+  for f in "${!p}"/*.Rmd; do
+    rmdFiles=("${rmdFiles[@]}" "$f")
+  done
+done
+
+rg -o "library\((\w+)\)" -r '$1' "${rmdFiles[@]}" | # find all 'library' statements and get package
   sed 's/.*://' | # remove filename prefix
   awk '!_[$0]++' > requirements.txt # unique
 
 while IFS=" " read -r package; 
 do 
-  Rscript -e "install.packages('"$package"', repos = 'http://cran.us.r-project.org')";
+  Rscript -e "install.packages('$package', repos = 'http://cran.us.r-project.org')";
 done < "requirements.txt"
